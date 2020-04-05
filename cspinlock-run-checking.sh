2b6ad8ede3a211482485f757c2e4be5d19fadb6e
akhoroshev <arthoroshev@gmail.com>
2020-04-05 15:16:32 +0300

    Add genmc output in README

--- cspinlock-run-checking.sh
+++ cspinlock-run-checking.sh
@@ -1,9 +1,13 @@
 DIRECTORY=`dirname $0`
-cd $DIRECTORY/genmc
 
+cd $DIRECTORY
+git submodule init
+git submodule update
+
+cd genmc
 autoreconf --install
 ./configure
-make
+make -j 4
 
 echo "Validate cspinlock/main.c..."
 ./src/genmc -unroll=8 ../cspinlock/main.c
